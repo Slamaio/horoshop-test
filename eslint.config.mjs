307c19fd54d8608e50aa0f1e78fd5d6c377a0520
Viacheslav Maior <maior.v@digitalite.org>
2025-07-22 18:26:49 +0300

    refactor: clean up eslint configuration by removing commented code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,9 @@
 import antfu from '@antfu/eslint-config'
-
-// export default antfu({
-//   formatters: true,
-//   vue: true,
-// })
-
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
   antfu({
     formatters: true,
     vue: true,
-  })
+  }),
 )
